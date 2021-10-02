e065dfb86b213d96b83d464b9c4e367d2b3cdc2b
Ibrahim Khajanchi <ibrahimkhajanchi@nbp-199-13.nbp.ruw.rutgers.edu>
2021-10-02 18:12:21 -0400

    made some improvements

--- A*.py
+++ A*.py
@@ -5,22 +5,20 @@ sys.setrecursionlimit(10000)
 class Node:
     def __init__(self):
         self.explored = False
-        self.blocked = None
+        self.blocked = False
         self.g = None
         self.h = None
 
 #initialize graph and call DFS to set nodes to blocked/unblocked, then return graph
 def createGraph():
     graph = [[Node() for j in range(101)] for i in range(101)]
-    graph = DFS(graph)
+    DFS(graph)
     return graph
 
 def DFS(graph):
     for x in range(101):
          for y in range(101):
-             if graph[x][y].explored == True:
-                 continue
-             else:
+             if not graph[x][y].explored:
                  graph = explore(graph, x, y) 
     return graph          
 
@@ -29,40 +27,28 @@ def explore(graph, x, y):
     #set current node to explored, set it to blocked with 30% probability and unblocked with 70% probability
     # if it is blocked, return graph without exploring neighbors
     # if it is unblocked, explore neighors
-    if graph[x][y].explored:
-        return graph
-
     graph[x][y].explored = True
-    if random.random() < .7:
-        graph[x][y].blocked = False
-    else:
+    if random.random() < .3:
         graph[x][y].blocked = True
-        return graph
+        
     #at this point the node is unblocked, so add neighbors to openList
     openList = []
-    # check y+1
-    if (y+1) <= 100:
-        if not graph[x][y+1].explored:
-            openList.append([x, y+1])
-    #check y-1
-    if (y-1) >= 0:
-        if not graph[x][y-1].explored:
-            openList.append([x, y-1])
-    #check x+1
-    if (x+1) <= 100:
-        if not graph[x+1][y].explored:
-            openList.append([x+1, y])
-    #check x-1
-    if (x-1) >= 0:
-        if not graph[x-1][y].explored:
-            openList.append([x-1, y])
+    xOffsets = [1, -1, 0, 0]
+    yOffsets = [0,0, -1, 1]
+
+    for i in range(4):
+        newX = x + xOffsets[i]
+        newY = y + yOffsets[i]
+
+        if newX >= 0 and newX < 101 and newY >= 0 and newY < 101 and (graph[newX][newY].explored == False):
+            openList.append([newX, newY])
+    
     #explore the items in the open list in random order
     while openList:
         neighbor = random.choice(openList)
         graph = explore(graph, neighbor[0], neighbor[1])
         openList.remove(neighbor)
-    #finally, return graph once all neighbors have been explored
-    return graph
+
 
 
 def create50graphs():
@@ -87,15 +73,7 @@ def graphAsString(graph):
         string = string + '\n'
     return string
 
-#prints graph for testing purposes
-def printGraph(graph):
-    for x in range(101):
-        for y in range(101):
-            if graph[x][y].blocked == False:
-                print('_', end='')
-            else:
-                print('X', end='')
-        print('')
+
 
 def main():
     create50graphs()
